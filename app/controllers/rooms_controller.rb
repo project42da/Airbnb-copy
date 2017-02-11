4df9591400cb2ff17e1116bff9aff4a0ac7bdea0
민우 천 <cmw9101@konkuk.ac.kr>
2017-02-11 23:38:23 +0900

    Make Hosting(rooms/new)
    - rooms/new (숙소등록기능을 만든다.)
    - nav-bar에 호스팅하기에 route를 연결한다.
    
    어려웠던부분
    - 나중에 스타일을 고려하여 마크업을 짜는것이 어려웠다.
    - 이렇게 해놔도 결국 어려울것같다.

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,6 @@
 class RoomsController < ApplicationController
   before_action :set_room, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user, except: [:show]
+  before_action :authenticate_user!, except: [:show]
 
   def index
     @rooms = Room.all
