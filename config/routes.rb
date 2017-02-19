4d1332cac5c15d489a741368364e761eeea6bb02
민우 천 <cmw9101@konkuk.ac.kr>
2017-02-19 22:26:45 +0900

    Make Reservation Model
    - user:references room:references 로 설정되어 있기 때문에, user,room모델에 has_many :reservations를 넣어준다.
    - rooms라우트 안에 reservations를 네스팅해준다.

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   resources :users, only: [:show]
-  resources :rooms
+
+  resources :rooms do
+    resources :reservations, only: [:create]
+  end
+  
 	resources :photos
 	  
 end
